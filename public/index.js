5f1227be9630f8ea0b674eded8ef0583a145cf7c
Brandon Stiles <brandonstiles@Brandons-MacBook-Pro.local>
2017-06-23 05:55:27 -0700

    deploy ready

---
+++ public/index.js
@@ -0,0 +1,643 @@
+$(document).ready(function() {
+
+  var map = L.map('map').setView([34, -85], 5);
+  var currentBaseMap = 'Oceans';
+  var baseLayer = L.esri.basemapLayer(currentBaseMap).addTo(map);
+
+  var markers;
+
+  var markerMap = {};
+  var markerGroup = [];
+
+  var nav = $('#sideNav');
+  $(".articles").hide();
+
+
+  $('#getLocation').on('click', function(event) {
+
+    event.preventDefault();    
+
+    $('#getLocation').html('Getting location...');
+
+    var options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0
+    };
+
+    function success(pos) {
+
+      $('#getLocation').html('Get My Current Location');
+
+      var crd = pos.coords;
+
+      $('#latitudeSearch').val(crd.latitude);
+      $('#longitudeSearch').val(crd.longitude);
+
+      var redIcon = new L.Icon({
+        iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+      });
+
+      var marker = L.marker([crd.latitude, crd.longitude], {icon: redIcon}).addTo(map);
+
+      marker.bindPopup('<h4>My Location</h4><p><span>Latitude: ' + crd.latitude + '</span><br><span>Longitude: ' + crd.longitude + '</span></p>');
+
+    };
+
+    function error(err) {
+      $('#getLocation').html("Can't get current location.");
+      console.warn('error on get current position', err.code, err.message);
+    };
+
+    navigator.geolocation.getCurrentPosition(success, error, options);
+
+  })
+
+
+  $('#clearMarkers').on('click', function(event) {
+      
+      event.preventDefault(); 
+
+      if(markers) {     
+        markers.clearLayers();
+      }
+
+      $('#resultsList').empty();
+      $(this).closest('form').find("input").val("");
+    
+      markerMap = {};
+
+  }); 
+
+
+  $(document).on('click', '.listItem', function() {
+
+     var id = $(this).attr('value');
+
+     var marker = markerMap[id];
+
+     marker.openPopup();
+
+     map.panTo(new L.LatLng( marker.getLatLng().lat, marker.getLatLng().lng), 8);
+
+  });
+
+
+  $(document).on('click', '#swapMapButton', function(event) {
+    
+    event.preventDefault();     
+
+    if(currentBaseMap === 'Oceans') {
+      currentBaseMap = 'Imagery';
+      setBasemap(currentBaseMap);
+
+    } else {
+      
+      currentBaseMap = 'Oceans';
+      setBasemap(currentBaseMap);
+
+    }
+
+  });
+
+  $(document).on('click', '.times', function(event) {
+
+    event.preventDefault();    
+    $(".articles").show();
+    $(".article-plus").hide();
+    $(".article-minus").show();
+    $(".article-content").show();
+    $(".articles").css("width", "100%");
+
+     var id = $(this).attr('value');
+   
+    $.ajax({
+
+      url: '/id',
+      method: 'GET',
+      data: {
+        id: id
+      }
+
+    }).done(function(res) {
+
+        var searchTerm = res[0].properties.vesslterms;
+
+        var start = parseDescriptionDates(res[0].properties.history);
+
+        console.log('search', searchTerm);
+        console.log('date', start);
+
+        var timesKey = "44c44dc78c634b63b56fcceefdbc86ef";
+
+        var timesURLBase = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" +
+          timesKey + "&q=";
+      
+        let timesURL = timesURLBase;
+        timesURL += searchTerm;
+
+        $.ajax( {
+          url: timesURL,
+          method: "GET",
+          data: {
+            startDate: start,
+            endDate: null
+          }
+          }).done( function(response) {
+            if(response!=null){
+              console.log(response);
+              let articleTitles = [];
+              let articleParagraphs = [];
+              let articleLinks = [];
+
+              let articleNum = response.response.docs.length;
+              for(var i = 0; i < articleNum; i++){
+                articleTitles[i] = response.response.docs[i].headline.main;
+                articleParagraphs[i] = response.response.docs[i].lead_paragraph;
+                articleLinks[i] = response.response.docs[i].web_url;
+
+                var $titleElement = $("<h4>");
+                var $paragraphElement = $("<p>");
+                var $linkElement = $("<a>Full Article</a>");
+
+                $titleElement.html(articleTitles[i]);
+                $paragraphElement.html(articleParagraphs[i]);
+                $linkElement.attr("href", articleLinks[i]).attr("target", "_blank");
+
+                $("#times-content").append($titleElement).append($paragraphElement).append($linkElement).append($("<hr>"));
+              }
+              console.log(articleTitles, articleParagraphs, articleLinks);
+            }
+          });
+      });
+
+  });
+
+  $(document).on('click', '.wiki', function(event) {
+
+    event.preventDefault();    
+    $(".articles").show();
+    $(".article-plus").hide();
+    $(".article-minus").show();
+    $(".article-content").show();
+    $(".articles").css("width", "100%");
+
+     var id = $(this).attr('value');
+
+     console.log('wiki', id);
+
+
+      $.ajax({
+
+        url: '/id',
+        method: 'GET',
+        data: {
+          id: id
+        }
+
+      }).done(function(res) {
+
+          var searchTerm = res[0].properties.vesslterms;
+
+          console.log(searchTerm);
+
+          let wikiURL = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerm + "&format=json&callback=?";
+        
+          $.ajax( {
+              url: wikiURL,       
+              type: 'GET',
+              dataType: 'json',
+              data: function(data, status, jqXHR) {
+                console.log(data);
+              }
+
+            }).done( function(response) {
+              let articleTitles = [];
+              let articleParagraphs = [];
+              let articleLinks = [];
+
+              console.log(response);
+              let articleNum = response[1].length;
+              for(var i = 0; i < articleNum; i++){
+              articleTitles[i] = response[1][i];
+              articleParagraphs[i] = response[2][i];
+              articleLinks[i] = response[3][i];
+
+              var $titleElement = $("<h4>");
+              var $paragraphElement = $("<p>");
+              var $linkElement = $("<a>Full Article</a>");
+
+              $titleElement.html(articleTitles[i]);
+              $paragraphElement.html(articleParagraphs[i]);
+              $linkElement.attr("href", articleLinks[i]).attr("target", "_blank");
+
+              $("#wiki-content").append($titleElement).append($paragraphElement).append($linkElement).append($("<hr>"));
+              }
+            });
+        });
+
+  });
+
+
+  $(document).on('click', '.congress', function(event) {
+
+    event.preventDefault();    
+    $(".articles").show();
+    $(".article-plus").hide();
+    $(".article-minus").show();
+    $(".article-content").show();
+    $(".articles").css("width", "100%");
+
+    var id = $(this).attr('value');
+  
+    console.log('congress', id);
+
+   $.ajax({
+
+        url: '/id',
+        method: 'GET',
+        data: {
+          id: id
+        }
+
+      }).done(function(res) {
+
+        let congressURL = "https://loc.gov/pictures/search/";
+        let query = res[0].properties.vesslterms;
+
+        $.getJSON(congressURL, {
+        
+          type: "search",
+          q: query.replace(/ /g, "%20"),
+          fo: 'json'
+        
+        }).done(function(results) {
+          let articleTitles = [];
+          let printLinks = [];
+          let articleDescriptions = [];
+          
+          articleNum = results.results.length;
+          for(var i = 0; i< articleNum; i++){
+            articleTitles[i] = results.results[i].subjects[0];
+            printLinks[i] = results.results[i].image.full.substring(2);
+            articleDescriptions[i] = results.results[i].title;
+            console.log(printLinks[i]);
+
+            var $titleElement = $("<h4>");
+            var $linkElement = $("<a>See Print</a>");
+            var $descriptionElement = $("<p>");
+
+            $titleElement.html(articleTitles[i]);
+            $linkElement.attr("href", printLinks[i]).attr("target", "_blank");
+            $descriptionElement.html(articleDescriptions[i]);
+
+            $("#congress-content").append($titleElement).append($descriptionElement).append($linkElement).append($("<hr>"));
+          }
+        });
+
+    });
+  });
+
+  $(document).on('click', '#clear-articles', function(){
+    $("#times-content").empty();
+    $("#wiki-content").empty();
+    $("#congress-content").empty();
+  });
+
+
+
+  map.on('dblclick', function(e) {
+
+    var popupSearch = '<h5> Latitude: ' + e.latlng.lat + '</br> Longitude: ' + e.latlng.lng + '</h5><form id="searchPopup" class="form-inline"> <p class="help-block">Search for wrecks within</p> <div class="form-group"><input type="number" class="form-control" id="milesInput" placeholder="50 miles"><button id="popupSearchButton" type="submit" class="btn btn-primary">Go</button> </div> </form>'
+    
+    var popup = L.popup()
+      .setLatLng(e.latlng)
+      .setContent(popupSearch)
+      .openOn(map);
+  
+
+    $('#popupSearchButton').on('click', function(event) {
+      
+      map.closePopup();
+
+      event.preventDefault();     
+
+      var mileRadius = $('#milesInput').val().trim();
+      
+      if(markers) {
+        markers.clearLayers();
+      }
+
+      $('#resultsList').empty();
+
+      markerMap = {};
+
+      $.ajax({
+
+        url: '/proximity',
+        method: 'GET',
+          data: {
+            lat: e.latlng.lat,
+            lon: e.latlng.lng,
+            radius: mileRadius * 1609
+          } 
+
+      }).done(function(res) {
+
+          buildMarkers(res);
+
+          buildList(res);
+
+        });
+
+      }); 
+
+  });
+
+
+  function setBasemap(basemap) {
+      
+      console.log(baseLayer);
+
+      if (baseLayer) {
+        map.removeLayer(baseLayer);
+      }
+
+      baseLayer = L.esri.basemapLayer(basemap);
+
+      map.addLayer(baseLayer);
+
+    }
+
+
+  var buildMarkers = function(res) {
+
+    markerMap = {};
+
+    markerGroup = [];
+
+    if(markers) {     
+    
+        markers.clearLayers();
+    
+    }
+
+
+    for(var i = 0; i < res.length; i++) {
+          
+      var name = res[i].properties.vesslterms;
+      
+      var location = res[i].geometry.coordinates[1] + ',' + res[i].geometry.coordinates[0];
+
+      var history = res[i].properties.history;
+
+      var favButton = '<br><button id="' + res[i]._id + '" class="favorite btn btn-warning btn-sm"> <span class="glyphicon glyphicon-star-empty"> </span> Add Favorite </button>';
+
+      var nytButton = '<button class="times research btn btn-success btn-sm" value=' + res[i]._id + '><span class="glyphicon glyphicon-folder-open"></span> NY Times </button>';
+
+      var wikiButton = '<button class="wiki research btn btn-success btn-sm" value=' + res[i]._id + '><span class="glyphicon glyphicon-folder-open"></span> Wikipedia</button>';
+
+      var locButton = '<button class="congress research btn btn-success btn-sm" value=' + res[i]._id + '><span class="glyphicon glyphicon-folder-open"></span> LOC </button>';
+
+      var swapoutMap = '<button id="swapMapButton" class="btn btn-warning btn-sm satButton">Toggle Base Map</button>';
+
+
+      if(name === '') name = 'Not Named';
+
+      if(history === '') history = 'No known history in database'
+
+
+      var marker = L.marker( [ res[i].geometry.coordinates[1], res[i].geometry.coordinates[0] ]);
+
+      marker.bindPopup('<h3><em>' + name + '</em></h3><h4>' + location + '</h4>' + history + favButton + swapoutMap + nytButton + wikiButton + locButton);
+    
+      markerGroup.push(marker);
+
+      markerMap[res[i]._id] = marker;
+
+    }
+
+
+
+    markers = L.layerGroup(markerGroup);
+
+    map.addLayer(markers);
+
+  };
+
+
+  var buildList = function(res) {
+
+    for(var i = 0; i < res.length; i++) {
+      
+      var item = $('<div class="list-group-item listItem">');
+      
+      item.attr('value', res[i]._id);
+      
+      var name = $('<h3>').css('font-style', 'italic').html(res[i].properties.vesslterms);
+      
+      var location = $('<h4>').html(res[i].geometry.coordinates[1] + ', ' + res[i].geometry.coordinates[0]); 
+      
+      var desc = $('<div>').css('font-style', 'initial').html(res[i].properties.history);
+
+      item.append(name);
+      item.append(location);
+      item.append(desc);
+
+      $('#resultsList').append(item);
+
+    }
+
+  };
+
+
+  var gotoMarker = function(id) {
+
+     var marker = markerMap[id];
+
+     marker.openPopup();
+
+     map.panTo(new L.LatLng( marker.getLatLng().lat, marker.getLatLng().lng), 8);
+
+  }
+
+var parseDescriptionDates = function(str) {
+
+  var dateRegEx = /((0?[1-9]|10|11|12)(-|\/)([0-9]|(0[0-9])|([12])([0-9]?)|(3[01]?))(-|\/)((\d{4})|(\d{2}))|(0?[2469]|11)(-|\/)((0[0-9])|([12])([0-9]?)|(3[0]?))(-|\/)((\d{4}|\d{2})))/g;
+  var sunkRegEx = /([Ss][Uu][Nn][Kk]) ((0?[1-9]|10|11|12)(-|\/)([0-9]|(0[0-9])|([12])([0-9]?)|(3[01]?))(-|\/)((\d{4})|(\d{2}))|(0?[2469]|11)(-|\/)((0[0-9])|([12])([0-9]?)|(3[0]?))(-|\/)((\d{4}|\d{2})))/g;
+
+  let dates = str.match(dateRegEx);
+
+        var sinkDate = null;
+
+        if(dates!==null){
+
+            if(dates.length == 2){
+                sinkDate = dates[0];
+            }else{
+            
+                dates = str.match(sunkRegEx);
+                console.log(dates);
+                sinkDate = dates[0].split(" ")[1];
+            
+            }
+            
+            let dateExpansion = sinkDate.split("/");
+            let sinkYear = dateExpansion[2];
+            
+            if (sinkYear.length==2){
+
+                if(parseInt(sinkYear) < 17){
+                    sinkYear = "20" + sinkYear;
+              
+                }else{
+              
+                    sinkYear = "19" + sinkYear;
+              
+                }
+            }
+            
+            let sinkMonth = dateExpansion[0];
+            
+            if(sinkMonth.length==1){
+           
+                sinkMonth = "0" + sinkMonth;
+           
+            }
+           
+            let sinkDay = dateExpansion[1];
+           
+            if(sinkDay.length==1){
+           
+                sinkDay = "0" + sinkDay;
+           
+            }
+
+            sinkDate = sinkYear + sinkMonth + sinkDay;
+        }
+
+        return(sinkDate);
+
+}
+
+
+  $('#openNav').on('click', function() {
+    nav.css('width', '27%');
+  });
+
+
+  $('#closeNav').on('click', function() {
+    nav.css('width', "0%");
+  });
+
+
+
+  $('#searchSubmit').on('click', function(event) {
+
+    event.preventDefault();
+
+    if(markers) markers.clearLayers();
+  
+    $('#resultsList').empty();
+
+    markerMap = {};
+
+    var name = $('#nameSearch').val().trim().toUpperCase();
+
+    var string = $('#textSearch').val().trim();
+
+    var lat = $('#latitudeSearch').val().trim();
+    var lon = $('#longitudeSearch').val().trim();
+    var radius = $('#radiusSearch').val().trim();
+
+    var after = $('#afterRangeSearch').val().trim();
+    var before = $('#beforeRangeSearch').val().trim();
+
+    var hasName = $('#hasName').prop('checked');
+    var hasHistory = $('#hasHistory').prop('checked');
+
+    var id = $('#idSearch').val().trim();
+
+
+    if(id) {
+
+      $.ajax({
+
+        url: '/id',
+        method: 'GET',
+        data: {
+          id: id
+        }
+
+      }).done(function(res) {
+
+        buildMarkers(res);
+
+        buildList(res);
+
+      })
+    
+    } else 
+
+      {
+
+        $.ajax({
+          url: '/wreck',
+          method: 'GET',
+          data: {
+
+            location: {
+                lat: lat,
+                lon: lon,
+                radius: radius * 1609  
+              },
+
+              name: name,
+
+              before: before,
+              after: after,
+
+              hasName: hasName,
+
+              string: string
+          }
+          }).done(function(res) {
+
+              buildMarkers(res);
+
+              buildList(res);
+
+            })
+      }
+  })
+
+      $(document).on("click", ".go-map", function() {
+        var id = $(this).attr("data-id");
+
+        $.ajax({
+
+          url: '/id',
+          method: 'GET',
+          data: {
+            id: id
+          }
+
+        }).done(function(res) {
+
+          buildMarkers(res);
+
+          gotoMarker(res[0]._id);
+
+          buildList(res);
+
+        })
+        
+      })
+
+})
+
---
+++ server.js
@@ -0,0 +1,334 @@
+
+
+const express   = require('express');
+const http      = require('http');
+const server    = http.createServer(app);
+const path      = require('path');
+const cors      = require('cors');
+
+
+var PORT = process.env.PORT || 8080;
+var app = express();
+app.use(cors());
+
+
+var mongo       = require('mongodb').MongoClient, test = require('assert');
+const ObjectID  = require('mongodb').ObjectID;
+
+var DB_STRING       = process.env.MONGODB_URI || "mongodb://localhost/tempest_db";
+var DB_COLLECTION   = 'wrecks';
+var collection;
+
+var resultsArr = [];
+
+
+app.listen(PORT, function() {
+  console.log('Web server listening on port ' + PORT);
+});
+
+app.use(express.static(__dirname + "/public"));
+
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "./index.html"));
+});
+
+
+app.get('/id', function (req, res) {
+
+  var qId = new ObjectID(req.query.id);
+
+  console.log('qid:', qId);
+
+  collection.findOne( { _id : qId }, function(err, doc) {
+
+    console.log('error', err);
+
+    console.log(doc)
+
+    res.send(doc);
+
+  })
+
+})
+
+
+app.get('/string', function (req, res) {
+
+  console.log('hit string endpoint');
+
+  var re = req.query.string;
+
+  var query = {};
+
+  var field = 'properties.history';
+
+    var operator = {};
+     
+    operator['$regex'] = re;
+    operator['$options'] = 'i';
+
+    query[field] = operator;
+
+    resultsArr = collection.find(query).toArray();
+
+    
+    resultsArr.then(function(arr) {
+
+      res.send(arr);
+
+    })
+
+})
+
+
+app.get('/all', function (req, res) {
+
+
+  collection.find().toArray( function( err, docs) {
+
+      resultsArr.push(docs);
+
+    });
+
+    res.send(resultsArr);
+
+});
+
+
+app.get('/range', function (req, res) {
+
+  console.log('range endpoint hit');
+
+  var before = parseInt(req.query.before);
+  var after = parseInt(req.query.after);
+
+  resultsArr = collection.find(
+    {
+      "properties.yearsunk": { $gt: after, $lt: before }
+    }
+    ).toArray();
+
+  resultsArr.then(function(arr) {
+
+    console.log(arr);
+
+    res.send(arr);
+
+  })
+
+})
+
+
+app.get('/wreck', function(req, res) {
+
+  console.log('hit wreck endpoint');
+
+  var query = {};
+
+
+  if(req.query.string) {
+
+    var re = req.query.string;
+
+    var field = 'properties.history';
+
+    var operator = {};
+     
+    operator['$regex'] = re;
+    operator['$options'] = 'i';
+
+    query[field] = operator;
+
+  }
+
+
+  if(req.query.before && req.query.after) {
+
+    var before = parseInt(req.query.before);
+    var after = parseInt(req.query.after);
+
+    var field = 'properties.yearsunk';
+
+    var operator = {};
+     
+    operator['$lt'] = before;
+    operator['$gt'] = after;
+
+    query[field] = operator;
+
+  }
+
+  if(req.query.before && !req.query.after) {
+    
+    var before = parseInt(req.query.before);
+    var field = 'properties.yearsunk';
+
+    var operator = {};
+    operator['$lt'] = before;
+
+    query[field] = operator;
+
+  }
+
+  if(req.query.after && !req.query.before) {
+    
+    var after = parseInt(req.query.after);
+    var field = 'properties.yearsunk';
+
+    var operator = {};
+    operator['$gt'] = after;
+
+    query[field] = operator;
+
+  }
+
+
+  if(req.query.location.lat && req.query.location.lon && req.query.location.radius) {
+    
+    var field = 'geometry';
+
+    var operator1 = {};
+    var operator2 = {};
+    var operator3 = {};
+
+    operator3['type'] = "Point";
+    operator3['coordinates'] = [ parseFloat(req.query.location.lon), parseFloat(req.query.location.lat) ];
+
+    operator2['$geometry'] = operator3;
+
+    operator2['$maxDistance'] = parseFloat(req.query.location.radius);
+
+    operator1['$near'] = operator2;
+
+    query[field] = operator1;
+
+  };
+
+
+  if(parseInt(req.query.hasName) === 1) { 
+
+    var field = 'properties.vesslterms';
+
+    var operator = {};
+    operator['$nin'] = ["", "UNKNOWN", "WRECK"];
+
+    query[field] = operator;
+
+  };
+
+  
+  if(req.query.name){
+  
+    var field = 'properties.vesslterms';
+
+    query[field] = req.query.name;
+  
+  }
+
+
+  resultsArr = collection.find(query).toArray();
+
+
+  resultsArr.then(function(arr) {
+
+    res.send(arr);
+
+  })
+
+})
+
+
+app.get('/hasname', function (req, res) {
+
+
+  resultsArr = collection.find(
+    {
+      "properties.vesslterms" : { $nin: ["", "UNKNOWN", "WRECK"] }   
+    }
+    ).toArray();
+
+
+  resultsArr.then(function(arr) {
+
+    res.send(arr);
+
+  })
+
+
+})
+
+
+app.get('/name', function (req, res) {
+
+  var shipName = req.query.name;
+
+  resultsArr = collection.find(
+    {
+      "properties.vesslterms": shipName.toUpperCase()
+    }
+    ).toArray();
+
+  resultsArr.then(function(arr) {
+
+    res.send(arr);
+
+  })
+
+})
+
+
+app.get('/proximity', function (req, res) {
+
+ 	var lat = parseFloat(req.query.lat);
+	var lon = parseFloat(req.query.lon);
+	var radius = parseFloat(req.query.radius);
+
+  var resultsArr = collection.find( 
+
+          {
+          	"geometry" : {
+          		
+          		$near : { 
+
+          			$geometry : {
+          			
+  	        			type : "Point" ,
+  	        			coordinates : [ lon, lat ]
+  	        		},
+
+          			$maxDistance : radius
+         			}
+
+         		}
+
+          },
+          
+          { 
+
+          }
+          ).toArray();
+
+  resultsArr.then(function(arg) {
+
+    res.send(arg);
+
+  });
+
+})
+         
+
+
+  mongo.connect(DB_STRING, function(err, db) {
+    
+    if(err) console.log('Error connection to MongoDB', err);
+    
+    console.log('Connected to MongoDb at', DB_STRING);
+
+    collection = db.collection(DB_COLLECTION);
+  
+    global_db = db;
+  
+  });
+
+
